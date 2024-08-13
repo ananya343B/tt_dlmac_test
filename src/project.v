4217b28e0c01d59d16afbc0a7835755217b9a76e
ananya343B <142582353+ananya343B@users.noreply.github.com>
2024-08-13 21:43:05 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -47,8 +47,8 @@ module reg_wrapper(
 reg [1:0] state;
 reg [15:0] temp_data;
 
-	always @(posedge clk or posedge !rst_n) begin
-		if (!rst_n) begin
+  always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
         state <= 2'b00; 
         reg_a <= 16'b0;
         reg_b <= 16'b0;
@@ -110,7 +110,7 @@ endmodule
   output reg [15:0]c_out;
   wire [15:0]fprod,fadd;
   
-	 always @(posedge clk or posedge !rst_n) begin
+  always @(posedge clk) begin
     if(!rst_n) begin
       c_out<=0;
     end
@@ -139,25 +139,25 @@ module dlfloat_mult(a,b,c_mul,clk);
 
 
   always@(posedge clk) begin
-        ma <={1'b1,a[8:0]};
-        mb<= {1'b1,b[8:0]};
-        sa <= a[15];
-        sb <= b[15];
-        ea <= a[14:9];
-        eb <= b[14:9];
+        ma ={1'b1,a[8:0]};
+        mb= {1'b1,b[8:0]};
+        sa = a[15];
+        sb = b[15];
+        ea = a[14:9];
+        eb = b[14:9];
 
-        e_temp <= ea + eb - 31;
-        m_temp <= ma * mb;
+        e_temp = ea + eb - 31;
+        m_temp = ma * mb;
 
-        mant <= m_temp[19] ? m_temp[18:10] : m_temp[17:9];
-        exp <= m_temp[19] ? e_temp+1'b1 : e_temp;
+        mant = m_temp[19] ? m_temp[18:10] : m_temp[17:9];
+        exp = m_temp[19] ? e_temp+1'b1 : e_temp;
 
-        s<=sa ^ sb;
+        s=sa ^ sb;
          if( a==16'hFFFF | b==16'hFFFF ) begin
-        c_mul <=16'hFFFF;
+        c_mul =16'hFFFF;
       end
       else begin
-        c_mul <= (a==0 | b==0) ? 0 :{s,exp,mant};
+        c_mul = (a==0 | b==0) ? 0 :{s,exp,mant};
       end 
  
     end 
@@ -178,161 +178,158 @@ module dlfloat_adder(input clk, input [15:0]a1, input [15:0]b1, output logic[15:
   	
    
     
-	always@(*) begin
+  always@(*) begin
         //stage 1
-	     e1_80 <= a1[14:9];
-	     e2_80 <= b1[14:9];
-          m1_80 <= a1[8:0];
-	      m2_80 <= b1[8:0];
-          s1_80 <= a1[15];
-	      s2_80 <= b1[15];
+	     e1_80 = a1[14:9];
+	     e2_80 = b1[14:9];
+          m1_80 = a1[8:0];
+	      m2_80 = b1[8:0];
+          s1_80 = a1[15];
+	      s2_80 = b1[15];
         
           if (e1_80  > e2_80) begin
-             Num_shift_80           <= e1_80 - e2_80;
-             Larger_exp_80          <= e1_80;                     
-             Small_exp_mantissa_80  <= {1'b1,m2_80};
-             Large_mantissa_80      <= {1'b1,m1_80};
+             Num_shift_80           = e1_80 - e2_80;
+             Larger_exp_80          = e1_80;                     
+             Small_exp_mantissa_80  = {1'b1,m2_80};
+             Large_mantissa_80      = {1'b1,m1_80};
           end
         
           else begin
-            Num_shift_80           <= e2_80 - e1_80;
-            Larger_exp_80          <= e2_80;
-            Small_exp_mantissa_80  <= {1'b1,m1_80};
-            Large_mantissa_80      <= {1'b1,m2_80};
+            Num_shift_80           = e2_80 - e1_80;
+            Larger_exp_80          = e2_80;
+            Small_exp_mantissa_80  = {1'b1,m1_80};
+            Large_mantissa_80      = {1'b1,m2_80};
           end
  
 	      if (e1_80 == 0 | e2_80 ==0) begin
-	        Num_shift_80 <= 0;
+	        Num_shift_80 = 0;
 	      end
 	      else begin
-	        Num_shift_80 <= Num_shift_80;
+	        Num_shift_80 = Num_shift_80;
 	      end
       
         
         //stage 2
         // append 1 and shift
 	    if (e1_80 != 0) begin
-	      Small_exp_mantissa_80  <= (Small_exp_mantissa_80 >> Num_shift_80);
+	      Small_exp_mantissa_80  = (Small_exp_mantissa_80 >> Num_shift_80);
         end
 	    else begin
-	      Small_exp_mantissa_80 <= Small_exp_mantissa_80;
+	      Small_exp_mantissa_80 = Small_exp_mantissa_80;
 	    end
 
 	    if (e2_80!= 0) begin
-            Large_mantissa_80 <= Large_mantissa_80;
+            Large_mantissa_80 = Large_mantissa_80;
 	    end
 	    else begin
-	       Large_mantissa_80 <= Large_mantissa_80;
+	       Large_mantissa_80 = Large_mantissa_80;
 	    end	
               
        //stage 3
        //add the mantissas
                                                     
         if (Small_exp_mantissa_80  < Large_mantissa_80) begin
-		   S_mantissa_80 <= Small_exp_mantissa_80;
-		   L_mantissa_80 <= Large_mantissa_80;
+		   S_mantissa_80 = Small_exp_mantissa_80;
+		   L_mantissa_80 = Large_mantissa_80;
         end
         else begin
 			
-		   S_mantissa_80 <= Large_mantissa_80;
-		   L_mantissa_80 <= Small_exp_mantissa_80;
+		   S_mantissa_80 = Large_mantissa_80;
+		   L_mantissa_80 = Small_exp_mantissa_80;
         end       
                        
         Add_mant_80=11'b0;
 	
 	    if (e1_80!=0 & e2_80!=0) begin
 		   if (s1_80 == s2_80) begin
-        		Add_mant_80 <= S_mantissa_80 + L_mantissa_80;
+        		Add_mant_80 = S_mantissa_80 + L_mantissa_80;
 		    end else begin
-			   Add_mant_80 <= L_mantissa_80 - S_mantissa_80;
+			   Add_mant_80 = L_mantissa_80 - S_mantissa_80;
 		    end
 	    end	
  	    else begin
-		     Add_mant_80 <= L_mantissa_80;
+		     Add_mant_80 = L_mantissa_80;
 	    end
       
 	 //renormalization for mantissa and exponent
      //stage 4
         if (Add_mant_80[10] ) begin
-		   Add1_mant_80<= Add_mant_80 >> 1;
-		   renorm_exp_80 <= 4'd1;
+		   Add1_mant_80= Add_mant_80 >> 1;
+		   renorm_exp_80 = 4'd1;
 	    end
         else begin 
            if (Add_mant_80[9])begin
-		     renorm_shift_80 <= 0;
-		     renorm_exp_80 <= 0;		
+		     renorm_shift_80 = 0;
+		     renorm_exp_80 = 0;		
 	       end
            else if (Add_mant_80[8])begin
-		     renorm_shift_80 <= 8'd1; 
-		     renorm_exp_80 <= -1;
+		     renorm_shift_80 = 8'd1; 
+		     renorm_exp_80 = -1;
 	        end 
            else if (Add_mant_80[7])begin
-		      renorm_shift_80 <= 8'd2; 
-		      renorm_exp_80 <= -2;		
+		      renorm_shift_80 = 8'd2; 
+		      renorm_exp_80 = -2;		
 	       end  
            else if (Add_mant_80[6])begin
-		      renorm_shift_80 <= 8'd3; 
-		      renorm_exp_80 <= -3;		
+		      renorm_shift_80 = 8'd3; 
+		      renorm_exp_80 = -3;		
 	       end
            else if (Add_mant_80[5])begin
-		      renorm_shift_80 <= 8'd4; 
-		      renorm_exp_80 <= -4;		
+		      renorm_shift_80 = 8'd4; 
+		      renorm_exp_80 = -4;		
 	       end
            else if (Add_mant_80[4])begin
-		      renorm_shift_80 <= 8'd5; 
-		      renorm_exp_80 <= -5;		
+		      renorm_shift_80 = 8'd5; 
+		      renorm_exp_80 = -5;		
 	       end
            else if (Add_mant_80[3])begin
-		      renorm_shift_80 <= 8'd6; 
-		      renorm_exp_80 <= -6;		
+		      renorm_shift_80 = 8'd6; 
+		      renorm_exp_80 = -6;		
 	       end
            else if (Add_mant_80[2])begin
-		      renorm_shift_80 <= 8'd7; 
-		      renorm_exp_80 <= -7;		
+		      renorm_shift_80 = 8'd7; 
+		      renorm_exp_80 = -7;		
 	        end
            else if (Add_mant_80[1])begin
-		      renorm_shift_80 <= 8'd8; 
-		      renorm_exp_80 <= -8;		
+		      renorm_shift_80 = 8'd8; 
+		      renorm_exp_80 = -8;		
 	        end
            else if (Add_mant_80[0])begin
-		      renorm_shift_80 <= 8'd9; 
-		      renorm_exp_80 <= -9;		
+		      renorm_shift_80 = 8'd9; 
+		      renorm_exp_80 = -9;		
 	        end
            Add1_mant_80 = Add_mant_80 << renorm_shift_80;
         
         end
      
       
-        Final_expo_80 <=  Larger_exp_80 + renorm_exp_80;
+        Final_expo_80 =  Larger_exp_80 + renorm_exp_80;
 	
-        Final_mant_80 <= Add1_mant_80[8:0]; 
+        Final_mant_80 = Add1_mant_80[8:0]; 
 
 	   if (s1_80 == s2_80) begin
-		  Final_sign_80 <= s1_80;
+		  Final_sign_80 = s1_80;
 	   end 
 
 	   if (e1_80 > e2_80) begin
-		  Final_sign_80 <= s1_80;	
+		  Final_sign_80 = s1_80;	
 	   end else if (e2_80 > e1_80) begin
-		  Final_sign_80 <= s2_80;
+		  Final_sign_80 = s2_80;
 	   end
 	  else begin
 
 		if (m1_80 > m2_80) begin
-			Final_sign_80 <= s1_80;		
+			Final_sign_80 = s1_80;		
 		end else begin
-			Final_sign_80 <= s2_80;
+			Final_sign_80 = s2_80;
 		end
       end	
       if( a1==16'hFFFF | b1==16'hFFFF) begin
-        c_add <= 16'hFFFF;
+        c_add = 16'hFFFF;
       end
       else begin
-        c_add <= (a1==0 & b1==0)?0:{Final_sign_80,Final_expo_80,Final_mant_80};
+        c_add = (a1==0 & b1==0)?0:{Final_sign_80,Final_expo_80,Final_mant_80};
       end 
     end 
     
 endmodule
-
-
-
