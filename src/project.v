98d590602b229382f4d69724372fbf8eb3b9fdb8
ananya343B <142582353+ananya343B@users.noreply.github.com>
2024-08-18 14:30:47 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -4,6 +4,7 @@
  */
 // Code your design here
 `default_nettype none
+
 module tt_um_dlfloatmac (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
@@ -18,20 +19,21 @@ module tt_um_dlfloatmac (
 wire [15:0]data_in;
 wire [15:0] c;
 wire [15:0]wa,wb;
-wire write_en;
+
 wire [7:0] c_byte;
 
-assign uio_oe = write_en?8'b11111111:8'b00000000;
-assign uio_out = 8'b0;
 
+assign uio_oe = 8'b0;
+assign uio_out= 8'b0;
+  
 assign data_in = {uio_in,ui_in};
 
-
-reg_wrapper wrap(clk,rst_n,data_in,wa,wb,write_en);
+reg_wrapper wrap(clk,rst_n,data_in,wa,wb);
 dlfloat_mac MAC(clk,rst_n,wa,wb,c);
-  output_wrapper byte_out(clk,rst_n,c,c_byte);
-//assign uio_out = c[15:8];
-assign uo_out = c_byte;
+out_wrapper wrap2(clk,rst_n,c,c_byte); 
+
+
+assign uo_out = c_byte[7:0];
    
 endmodule
 
@@ -40,19 +42,17 @@ module reg_wrapper(
     input rst_n,
     input [15:0] data_in,
     output reg [15:0] reg_a,
-    output reg [15:0] reg_b,
-    output reg write_en
+    output reg [15:0] reg_b
 );
 
 reg [1:0] state;
 reg [15:0] temp_data;
 
-	always @(posedge clk or posedge !rst_n) begin
+  always @(posedge clk or negedge rst_n) begin
     if (!rst_n) begin
         state <= 2'b00; 
         reg_a <= 16'b0;
         reg_b <= 16'b0;
-        write_en <= 0;
     end
     else begin
         case (state)
@@ -65,7 +65,6 @@ reg [15:0] temp_data;
             2'b01: begin
                 reg_a <= temp_data;
                 reg_b <= data_in;
-                write_en <= 1;
                 state <= 2'b00;
             end
             default: state <= 2'b00; 
@@ -75,33 +74,35 @@ end
 
 endmodule
 
-module output_wrapper(input clk,
-                      input rst_n,
-                      input [15:0] c,
-                      output reg [7:0] c_byte);
-                      
-  reg state;
-  
- always @(posedge clk or negedge rst_n) begin
+module out_wrapper(
+    input clk,
+    input rst_n,
+  input [15:0] c,
+  output reg [7:0] c_byte
+);
+
+reg [1:0] state;
+
+  always @(posedge clk or negedge rst_n) begin
     if (!rst_n) begin
-        state <= 1'b0; 
-        c_byte<=7'b0;
-       
+        state <= 2'b00; 
+        c_byte <=8'b0;
     end
     else begin
         case (state)
-            1'b0: begin//msb
-              c_byte<=c[15:8];
-                state <= 1'b1;
+            2'b00: begin
+              c_byte <= c[7:0];
+                state <= 2'b01;
             end
-            1'b1: begin//lsb
-              c_byte<=c[7:0];
-                state <= 1'b0;
+            2'b01: begin
+              c_byte <= c[15:8]; 
+                state <= 2'b00;
             end
-            default: state <= 1'b0; 
+            default: state <= 2'b00; 
         endcase
     end
- end
+end
+
 endmodule
 
  module dlfloat_mac(clk,rst_n,a,b,c_out);
@@ -110,7 +111,7 @@ endmodule
   output reg [15:0]c_out;
   wire [15:0]fprod,fadd;
   
-	 always @(posedge clk or posedge !rst_n) begin
+  always @(posedge clk) begin
     if(!rst_n) begin
       c_out<=0;
     end
@@ -119,15 +120,15 @@ endmodule
 		end
   end
 	
-  dlfloat_mult mul(a,b,fprod,clk);
+	 dlfloat_mult mul(a,b,fprod,clk,rst_n);
   dlfloat_adder add(clk,fprod,c_out,fadd);
 
 endmodule 
 
-module dlfloat_mult(a,b,c_mul,clk);
+module dlfloat_mult(a,b,c_mul,clk,rst_n);
     input  [15:0]a,b;
-    input clk;
-  output  reg[15:0]c_mul=0;
+    input clk,rst_n;
+    output  reg[15:0]c_mul;
     
     reg [9:0]ma,mb; //1 extra because 1.smthng
     reg [8:0] mant;
@@ -135,65 +136,69 @@ module dlfloat_mult(a,b,c_mul,clk);
     reg [5:0] ea,eb,e_temp,exp;
     reg sa,sb,s;
     reg [16:0] temp;
+    reg [15:0] c_mul1;
    
-
-
-  always@(posedge clk) begin
+  always @(posedge clk) begin
+    if(!rst_n) begin
+      c_mul<=16'b0;
+    end
+    else begin
+      c_mul<=c_mul1;
+    end
+  end	
+	
+	always@(*) begin
         ma ={1'b1,a[8:0]};
         mb= {1'b1,b[8:0]};
         sa = a[15];
         sb = b[15];
         ea = a[14:9];
         eb = b[14:9];
-
+	
         e_temp = ea + eb - 31;
         m_temp = ma * mb;
-
+		
         mant = m_temp[19] ? m_temp[18:10] : m_temp[17:9];
         exp = m_temp[19] ? e_temp+1'b1 : e_temp;
-
+		
         s=sa ^ sb;
          if( a==16'hFFFF | b==16'hFFFF ) begin
-        c_mul =16'hFFFF;
+        c_mul1 =16'hFFFF;
       end
       else begin
-        c_mul = (a==0 | b==0) ? 0 :{s,exp,mant};
+        c_mul1 = (a==0 | b==0) ? 0 :{s,exp,mant};
       end 
  
     end 
 endmodule 
 
-module dlfloat_adder(input clk, input [15:0]a1, input [15:0]b1, output logic[15:0]c_add=0);
+module dlfloat_adder(input clk,input [15:0] a1, input [15:0] b1,output reg [15:0] c_add=0);
+
+   	
+    reg    [15:0] Num_shift_80; 
+    reg    [5:0]  Larger_exp_80,Final_expo_80;
+    reg    [9:0] Small_exp_mantissa_80,S_mantissa_80,L_mantissa_80,Large_mantissa_80;
+    reg    [8:0] Final_mant_80;
+    reg    [10:0] Add_mant_80,Add1_mant_80;
+    reg    [5:0]  e1_80,e2_80;
+    reg    [8:0] m1_80,m2_80;
+    reg          s1_80,s2_80,Final_sign_80;
+    reg    [8:0]  renorm_shift_80;
+    integer signed   renorm_exp_80;
+  	
+   
     
-  reg [15:0] Num_shift_80=16'b0; 
-  reg [5:0]  Larger_exp_80=6'b0;
-  reg [5:0] Final_expo_80=6'b0;
-  reg [9:0] Small_exp_mantissa_80=10'b0;
-  reg [9:0] S_mantissa_80=10'b0;
-  reg [9:0] L_mantissa_80=10'b0;
-  reg [9:0] Large_mantissa_80=10'b0;
-  reg [8:0] Final_mant_80=9'b0;
-  reg [10:0] Add_mant_80=11'b0;
-  reg [10:0] Add1_mant_80=11'b0;
-  reg [5:0]  e1_80=6'b0;
-  reg [5:0] e2_80=6'b0;
-  reg [8:0] m1_80=9'b0;
-  reg [8:0] m2_80=9'b0;
-  reg s1_80=1'b0;
-  reg s2_80=1'b0;
-  reg Final_sign_80=1'b0;
-  reg [8:0] renorm_shift_80=9'b0;
-  integer signed   renorm_exp_80=0;
-  
-	always@(a1 or b1) begin
+  always@(*) begin
         //stage 1
 	     e1_80 = a1[14:9];
 	     e2_80 = b1[14:9];
-          m1_80 = a1[8:0];
-	      m2_80 = b1[8:0];
-          s1_80 = a1[15];
-	      s2_80 = b1[15];
-        
+             m1_80 = a1[8:0];
+	     m2_80 = b1[8:0];
+             s1_80 = a1[15];
+	     s2_80 = b1[15];
+
+	  Num_shift_80=16'b0;
+	  
           if (e1_80  > e2_80) begin
              Num_shift_80           = e1_80 - e2_80;
              Larger_exp_80          = e1_80;                     
@@ -260,9 +265,11 @@ module dlfloat_adder(input clk, input [15:0]a1, input [15:0]b1, output logic[15:
       
 	 //renormalization for mantissa and exponent
      //stage 4
-	Add1_mant_80 = 0;
-	  renorm_exp_80=0;
-	  renorm_shift_80=0;
+	  //to avoid latch inference
+	   renorm_exp_80=4'd0;
+	   renorm_shift_80=9'd0;
+	   Add1_mant_80=Add1_mant_80;
+	  
         if (Add_mant_80[10] ) begin
 		   Add1_mant_80= Add_mant_80 >> 1;
 		   renorm_exp_80 = 4'd1;
@@ -312,11 +319,12 @@ module dlfloat_adder(input clk, input [15:0]a1, input [15:0]b1, output logic[15:
         
         end
      
-      
+        Final_expo_80 = 6'd0;//to avoid latch inference
         Final_expo_80 =  Larger_exp_80 + renorm_exp_80;
-	
+	 Final_mant_80 = 9'd0;//to avoid latch inference
         Final_mant_80 = Add1_mant_80[8:0]; 
 
+	  Final_sign_80=0;//to avoid latch inference
 	   if (s1_80 == s2_80) begin
 		  Final_sign_80 = s1_80;
 	   end 
